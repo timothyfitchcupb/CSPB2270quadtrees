1885db8294d7ec3b6e5f2d4a883d0fd5b16414e5
timothyfitchcupb <Timothy.Fitch@colorado.edu>
2024-08-10 02:33:18 +0000

    1st ImageData.h commit

---
+++ ImageData.h
@@ -0,0 +1,15 @@
+#ifndef IMAGE_DATA_H
+#define IMAGE_DATA_H
+
+#include <vector>
+
+std::vector<std::vector<int>> getImageData() {
+    return {
+        {10, 10, 20, 20},
+        {10, 10, 20, 20},
+        {30, 30, 40, 40},
+        {30, 30, 40, 40}
+    };
+}
+
+#endif // IMAGE_DATA_H
